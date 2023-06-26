42ea0fbe47e9f315d98cc725d043d8c2f5060938
limasavi <hezronkimariny@gmail.com>
2023-06-26 13:36:58 -0700

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ * @s: string
+ * Return: None
+ */
+
+void rev_string(char *s)
+{
+	int c = 0, d = 0;
+	char a;
+	
+	while (s[d++])
+	{
+	c++;
+	}
+	for (d = c - 1; d >= c / 2; d--)
+	{
+		a = s[d];
+		s[d] = s[c - d - 1];
+		s[c - d - 1] = a;
+	}
+}
