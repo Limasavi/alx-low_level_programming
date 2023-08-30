d4ebe69b315333d4bb36948a5d86dcd4220245f4
Limasavi <hezronkimariny@gmail.com>
2023-08-30 06:56:44 -0700

    0x08-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,10 @@
 #include <unistd.h>
 #include "main.h"
+/**
+ * _puts_recursion - Prints a string followed by a new line in  recursion
+ * @s: string
+ * return: none
+ */
 
 void _puts_recursion(char *s)
 {
