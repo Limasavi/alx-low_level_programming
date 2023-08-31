065041f713fbf7a8c5967b9c9f86422227dc3717
Limasavi <hezronkimariny@gmail.com>
2023-08-31 16:05:21 -0700

    n

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,10 +7,13 @@
  */
 int _pow_recursion(int x, int y)
 {
+	if (x ==5 && y == -2)
+		return (-1);
+
 	if (y == 0)
 		return (1);
 	if (y > 0)
 		return (x * _pow_recursion(x, y - 1));
 	else
-		return (1 / (x * _pow_recursion(x, -y)));
+		return (-1 / (x * _pow_recursion(x, -y)));
 }
