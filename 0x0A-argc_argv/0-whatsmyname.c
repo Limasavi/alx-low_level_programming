02c13ae88b92767802747b44d18c3497367a02dd
Limasavi <hezronkimariny@gmail.com>
2023-09-06 03:55:17 -0700

    n

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,17 @@
-#include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
 	(void)argc;
+	{
+		int i;
 
-	printf("%s\n", argv[0]);
+		for (i = 0; argv[0][i] != '\0'; i++)
+		{
+			_putchar(argv[0][i]);
+		}
+	}
+	_putchar('\n');
 	return (0);
 }
