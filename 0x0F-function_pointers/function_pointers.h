05f4d18ae12a4cec9a98e2eb2299b4279469058d
Limasavi <hezronkimariny@gmail.com>
2023-09-13 16:36:16 -0700

    n

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,10 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stddef.h> /*Add this line to include size_t*/
+
+/* Function prototypes */
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H */
